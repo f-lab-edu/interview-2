62bdecb9d3121d6fb14cf25fcc46cda8f478049a
gongdlida <jun931223@gmail.com>
2025-12-18 16:40:44 +0900

    refactor(components): 유틸 함수 추출 및 스타일 수정

--- src/features/ticket-list/index.tsx
+++ src/features/ticket-list/index.tsx
@@ -1,29 +1,26 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useNavigate } from 'react-router-dom'
 import type { Ticket } from '@/types/ticket'
 import { TicketCard } from '@/features/ticket-list/components'
-import { useNavigate } from 'react-router-dom'
 
 const TicketList = () => {
   const ticketList = useLoaderData<Ticket[]>()
   const navigate = useNavigate()
   return (
-    <div className='bg-neutral-50 min-h-screen pb-12'>
-      <div className='max-w-6xl mx-auto md:px-6'>
-        <div className='h-30 flex flex-col justify-center gap-2'>
-          <p className='text-3XL-Bold'>Available Events</p>
-          <p>Browse and select an event to book tickets</p>
-        </div>
-        <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>
-          {ticketList.map((ticketInfo) => {
-            return (
-              <TicketCard
-                key={ticketInfo.id}
-                onClick={() => navigate(`ticket/${ticketInfo.id}`)}
-                {...ticketInfo}
-              />
-            )
-          })}
-        </div>
+    <div className='max-w-6xl mx-auto md:px-6'>
+      <div className='h-30 flex flex-col justify-center gap-2'>
+        <p className='text-3XL-Bold'>Available Events</p>
+        <p>Browse and select an event to book tickets</p>
+      </div>
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>
+        {ticketList.map((ticketInfo) => {
+          return (
+            <TicketCard
+              key={ticketInfo.id}
+              onClick={() => navigate(`ticket/${ticketInfo.id}`)}
+              {...ticketInfo}
+            />
+          )
+        })}
       </div>
     </div>
   )
